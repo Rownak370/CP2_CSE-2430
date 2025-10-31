612b2356e9660c87ba618613d16d1ec5c06fbf31
Rownok_Nigar <rownakjahannigar@gmail.com>
2025-11-01 00:01:04 +0600

    positioninarray.cpp

--- positioninarray.cpp
+++ positioninarray.cpp
@@ -7,7 +7,7 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];  
-    
+    }
     for (int i = 0; i < n; i++) {
         if (a[i] <= 10) {  
             cout << "A[" << i << "] = " << a[i] << endl;
